8c09fcc850545210c0bfd0bc567bffd324aa7aae
Dario Vizzaccaro <dariovizz@hotmail.it>
2013-06-05 16:10:19 +0200

    ohhhh ouiiiii

--- Phase2/src/gui/SocialMain.java
+++ Phase2/src/gui/SocialMain.java
@@ -2,6 +2,8 @@ package gui;
 
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Side;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -23,41 +25,71 @@ public class SocialMain extends Application {
         
         AnchorPane root = new AnchorPane();
         primaryStage.setScene(new Scene(root));
-        TabPane tabPane = new TabPane();
-        tabPane.setPrefSize(450, 380);
+        final TabPane tabPane = new TabPane();
+        tabPane.setPrefSize(460, 380);
         tabPane.setSide(Side.TOP);
         tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
-        
+        final GridPane geoGrid = new GridPane();
+        geoGrid.setHgap(5); // Abstand links/rechts
+        geoGrid.setVgap(5); // Abstand oben/unten
+        final GridPane geoGrid2 = new GridPane();
+        geoGrid2.setHgap(5); // Abstand links/rechts
+        geoGrid2.setVgap(5); // Abstand oben/unten
         
         final Tab tab1 = new Tab();
         tab1.setText("Live Tickers");
-        
         
         final Tab tab2 = new Tab();
         tab2.setText("My Profile");
         
         Button joinTicker = new Button("Join");
         joinTicker.setMinWidth(50);
-        Button create3 = new Button("Create Ticker");
-        create3.setMinWidth(50);
-        Button create4 = new Button("Exit");
-        create4.setMinWidth(50);
+        joinTicker.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+               if(tabPane.getTabs().size() < 6){
+                 int i = tabPane.getTabs().size();
+                 Tab k = new Tab();
+                 tabPane.getTabs().add(i, k);
+                 tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.SELECTED_TAB);
+                 final GridPane geoGridk = new GridPane();
+                 geoGridk.setHgap(5); // Abstand links/rechts
+                 geoGridk.setVgap(5); // Abstand oben/unten
+                 
+                 Button sendchat = new Button("Send");
+                 sendchat.setMinWidth(50);
+                 
+                 geoGridk.add(sendchat, 12, 12);
+                 k.setContent(geoGridk);
+                 
+                 k.setText("Game"+i);
+               i++;
+               }
+            }
+        });
+        
+        Button create = new Button("Create Ticker");
+        create.setMinWidth(50);
+        Button exitmain = new Button("Exit");
+        exitmain.setMinWidth(50);
         Label tickertext = new Label("Choose the ticker you'd like to join");
         ListView ticklist = new ListView();
+       
         
-        final GridPane geoGrid = new GridPane();
-        geoGrid.setHgap(5); // Abstand links/rechts
-        geoGrid.setVgap(5); // Abstand oben/unten
+        Button test = new Button("Create Ticker");
+        create.setMinWidth(50);
         
         
         geoGrid.add(tickertext, 1, 1);
         geoGrid.add(ticklist, 1, 2);
         geoGrid.add(joinTicker, 1, 3);
-        geoGrid.add(create3, 2, 3);
-        geoGrid.add(create4, 12, 3);
+        geoGrid.add(create, 2, 3);
+        geoGrid.add(exitmain, 12, 3);
+        geoGrid2.add(test, 1, 1);
+        
         
         tab1.setContent(geoGrid);
-
+        tab2.setContent(geoGrid2);
         
         tabPane.getTabs().addAll(tab1, tab2);
         root.getChildren().add(tabPane);
