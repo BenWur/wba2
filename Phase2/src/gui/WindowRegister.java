7d2712ee710749c0af27a2cdeb4c79991593c31a
BenWur <benwurth@web.de>
2013-06-21 20:55:58 +0200

    also in register

--- Phase2/src/gui/WindowRegister.java
+++ Phase2/src/gui/WindowRegister.java
@@ -16,6 +16,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -24,87 +26,96 @@ public class WindowRegister extends Application {
 
     @Override
     public void start(final Stage primaryStage) {
+    	
+    	Image image1 = new Image("file:Logo/Logo.png");
+    	// simple displays ImageView the image as is
+        ImageView iv1 = new ImageView();
+        iv1.setLayoutX(5);
+        iv1.setLayoutY(5);
+        iv1.setFitWidth(290);
+        iv1.setImage(image1);
+    	
         primaryStage.setResizable(false);
 
         final Label errmessage = new Label();
         errmessage.setLayoutX(50);
-        errmessage.setLayoutY(325);
+        errmessage.setLayoutY(355);
 
         Label name = new Label("Username:");
         name.setLayoutX(50);
-        name.setLayoutY(31);
+        name.setLayoutY(61);
 
         final TextField textField = new TextField();
         textField.setLayoutX(50);
-        textField.setLayoutY(47);
+        textField.setLayoutY(77);
         textField.setMinWidth(200);
 
         Label fname = new Label("First name:");
         fname.setLayoutX(50);
-        fname.setLayoutY(73);
+        fname.setLayoutY(103);
 
         final TextField fnameField1 = new TextField();
         fnameField1.setLayoutX(50);
-        fnameField1.setLayoutY(89);
+        fnameField1.setLayoutY(119);
         fnameField1.setMinWidth(200);
 
         Label lname = new Label("Last name:");
         lname.setLayoutX(50);
-        lname.setLayoutY(115);
+        lname.setLayoutY(145);
 
         final TextField lnameField1 = new TextField();
         lnameField1.setLayoutX(50);
-        lnameField1.setLayoutY(131);
+        lnameField1.setLayoutY(161);
         lnameField1.setMinWidth(200);
 
         Label gname = new Label("Gender:");
         gname.setLayoutX(50);
-        gname.setLayoutY(157);
+        gname.setLayoutY(187);
 
         final ToggleGroup ggroup = new ToggleGroup();
         final RadioButton gbm = new RadioButton("Male");
         gbm.setToggleGroup(ggroup);
         gbm.setSelected(true);
         gbm.setLayoutX(70);
-        gbm.setLayoutY(173);
+        gbm.setLayoutY(203);
 
         final RadioButton gbf = new RadioButton("Female");
         gbf.setToggleGroup(ggroup);
         gbf.setLayoutX(155);
-        gbf.setLayoutY(173);
+        gbf.setLayoutY(203);
 
         Label bday = new Label("Birthday:");
         bday.setLayoutX(50);
-        bday.setLayoutY(199);
+        bday.setLayoutY(229);
 
         final TextField bdayField = new TextField();
         bdayField.setLayoutX(50);
-        bdayField.setLayoutY(215);
+        bdayField.setLayoutY(245);
         bdayField.setMinWidth(200);
         bdayField.setPromptText("YYYY-MM-DD");
 
         Label country = new Label("Country:");
         country.setLayoutX(50);
-        country.setLayoutY(241);
+        country.setLayoutY(271);
 
         Register register = new Register();
         final ChoiceBox countryc = new ChoiceBox(FXCollections.observableArrayList(register.countrylist()));
         countryc.getSelectionModel().selectFirst();
         countryc.setLayoutX(122);
-        countryc.setLayoutY(257);
+        countryc.setLayoutY(287);
 
         Label city = new Label("City:");
         city.setLayoutX(50);
-        city.setLayoutY(283);
+        city.setLayoutY(313);
 
         final TextField cityField = new TextField();
         cityField.setLayoutX(50);
-        cityField.setLayoutY(299);
+        cityField.setLayoutY(329);
         cityField.setMinWidth(200);
 
         Button backbtn = new Button();
         backbtn.setLayoutX(122);
-        backbtn.setLayoutY(371);
+        backbtn.setLayoutY(401);
         backbtn.setText("Back");
         backbtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -121,7 +132,7 @@ public class WindowRegister extends Application {
 
         Button btn = new Button();
         btn.setLayoutX(122);
-        btn.setLayoutY(345);
+        btn.setLayoutY(375);
         btn.setText("Register");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -154,6 +165,7 @@ public class WindowRegister extends Application {
         });
 
         AnchorPane root = new AnchorPane();
+        root.getChildren().add(iv1);
         root.getChildren().add(btn);
         root.getChildren().add(name);
         root.getChildren().add(fname);
@@ -173,7 +185,7 @@ public class WindowRegister extends Application {
         root.getChildren().add(cityField);
         root.getChildren().add(backbtn);
 
-        Scene scene = new Scene(root, 300, 400);
+        Scene scene = new Scene(root, 300, 430);
 
         primaryStage.setTitle("SocialTicker | Register");
         primaryStage.setScene(scene);
